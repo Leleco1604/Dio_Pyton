c4237dac507ebac1364c5fc7210e4d65954bc519
Leleco1604 <leonardogbarbosamelo2003@gmail.com>
2023-06-13 21:40:50 -0300

    Implementações no Projeto

--- Banco.py
+++ Banco.py
@@ -1,35 +1,13 @@
-menu = """"
-
-[d] depositar 
-[s] sacar 
-[e] extrato
-[q] sair 
-
-
-=> """
-
 saldo = 0
 limite = 500
 extrato = ""
 numero_saques = 0
+usuarios = []
 LIMITE_SAQUES = 3
 
-while True:
 
-    opcao = input(menu)
-
-    if opcao == "d":
-        print("Depósito")
-        valor = float(input("Qual é o o valor do desposito? "))
-
-        if valor > 0:
-            saldo += valor
-            extrato += f"Deposito: R$ {valor:.2f}\n"
-
-        else:
-            print("Operação falhou, o valor informado é invalido.")
-
-    elif opcao == "s":
+def sacar():
+        global saldo, extrato, numero_saques
         print("Saque")
         valor = float(input("Qual é o o valor do Saque? "))
        
@@ -56,13 +34,76 @@ while True:
         else:
             print("Operação falhou! O valor informado é invalido.")
 
-    elif opcao == "e":
-       print("\n ======================= EXTRATO =======================")
-       print("Não foram realizadas movimentações." if not extrato else extrato)
-       print("\n========================================================")
-       print("\nSaldo: R$ {}".format(saldo))
-       print("==========================================================")
 
+def depositar():
+    global saldo, extrato
+    print("Depósito")
+    valor = float(input("Qual é o o valor do desposito? "))
+
+    if valor > 0:
+            saldo += valor
+            extrato += f"Deposito: R$ {valor:.2f}\n"
+
+    else:
+            print("Operação falhou, o valor informado é invalido.")          
+         
+
+
+def exibir_extrato():
+    global extrato, saldo
+    print("\n ======================= EXTRATO =======================")
+    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print("\n========================================================")
+    print("\nSaldo: R$ {}".format(saldo))
+    print("==========================================================")
+
+
+def criar_usuario(usuarios):
+    cpf = input('Informe o seu CPF')
+    usuario = filtrar_usuarios(cpf,usuarios)
+
+    if usuario:
+        print("já existe usuario com esse CPF!")
+        return
+    nome = input("Informe o seu nome completo: ")
+    data_nascimento = input("Informe qual é a sua data de nascimento (dd-mm-aaaa): ")
+    endereco = input("Qual é o seu endereço: ")
+    
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
+
+    print("==== Usuario criado com sucesso! ====")
+
+def filtrar_usarios(cpf,usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] = cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
+
+         
+
+menu = """"
+
+[d] depositar 
+[s] sacar 
+[e] extrato
+[q] sair 
+[nu] novo usuario
+
+
+=> """
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "d":
+        depositar()
+    elif opcao == "s":
+        sacar()
+    elif opcao == "nu":
+         criar_usuario(usuarios)
+    elif opcao == "e":
+        exibir_extrato()
+    
     elif opcao == "q":
         break
     else:
